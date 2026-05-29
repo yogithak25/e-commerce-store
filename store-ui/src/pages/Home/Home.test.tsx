365cedff795c892b8783f8fd17b4148a2c0889ee
Ubuntu <ubuntu@ip-172-31-1-69.ap-south-2.compute.internal>
2026-05-29 09:30:44 +0000

    update

--- store-ui/src/pages/Home/Home.test.tsx
+++ store-ui/src/pages/Home/Home.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import Home from './Home';
 
-test('renders learn react link', () => {
-  render(<Home />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders home page', () => {
+  render(
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  );
+
+  expect(screen.getByText(/Shop by Category/i)).toBeInTheDocument();
 });
